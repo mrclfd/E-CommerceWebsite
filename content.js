a4d621d272fd765456926d28bba88cf03aed5772
Marcel Faraday <mrclfd@yahoo.com>
2023-11-24 11:15:17 +0700

    Update content.js

--- content.js
+++ content.js
@@ -29,7 +29,7 @@ function dynamicClothingSection(ob) {
   h4.appendChild(h4Text);
 
   let h2 = document.createElement("h2");
-  let h2Text = document.createTextNode("Rp  " + ob.price);
+  let h2Text = document.createTextNode("Rp  " + ob.price + 'K');
   h2.appendChild(h2Text);
 
   boxDiv.appendChild(boxLink);
